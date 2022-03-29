b2055407b18e491014c17cafbd147b038bd0a669
Loïc Labaisse <loic.labaisse@flit-sport.fr>
2022-03-29 11:46:25 +0200

    new router

---
+++ back/api/router/router.go
@@ -0,0 +1,14 @@
+package router
+
+import "github.com/gorilla/mux"
+
+type IGorillaRouter interface {
+	initRouter() *mux.Router
+}
+
+type router struct{}
+
+func (router *router) InitRouter() *mux.Router {
+	r := mux.NewRouter()
+	return r
+}
---
+++ back/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Starting server at port 4200\n")
+
+}
